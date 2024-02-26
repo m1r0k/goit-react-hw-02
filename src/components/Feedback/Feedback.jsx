6b6380749d2d0fc5d0dc5cad0d8806d16ed84b7a
m1r0k <mir_ok@ukr.net>
2024-02-26 17:30:41 +0200

    Optimization

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,18 +3,18 @@ import css from './Feedback.module.css';
 export default function Feedback ({clicks, totalClicks, persentOfPositive}) { 
     if (totalClicks === 0) { 
         return (
-        <div className={css.feedback}>
-            <p>No feedback yet</p>
-        </div>)
-    } else {
-        return (
-            <div>
-                <p>Good: {clicks.good}</p>
-                <p>Neutral: {clicks.neutral}</p>
-                <p>Bad: {clicks.bad}</p>
-                <p>Total: {totalClicks}</p>
-                <p>Positive: {persentOfPositive}%</p>
+            <div className={css.feedback}>
+                <p>No feedback yet</p>
             </div>
-        )
-    }
+        );
+    } 
+    return (
+        <div className={css.feedback}>
+            <p>Good: {clicks.good}</p>
+            <p>Neutral: {clicks.neutral}</p>
+            <p>Bad: {clicks.bad}</p>
+            <p>Total: {totalClicks}</p>
+            <p>Positive: {persentOfPositive}%</p>
+        </div>
+    );
 }
